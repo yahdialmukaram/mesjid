a7a59469b7b1c547c8d520d470d920d4ccbf823d
yahdi <yahdialmukaram03@gmail.com>
2020-05-26 23:50:32 +0700

    db

--- mesjid.sql
+++ mesjid.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Bulan Mei 2020 pada 15.12
+-- Waktu pembuatan: 26 Bulan Mei 2020 pada 18.49
 -- Versi server: 10.1.31-MariaDB
 -- Versi PHP: 7.2.3
 
@@ -41,10 +41,40 @@ CREATE TABLE `tb_infak` (
 --
 
 INSERT INTO `tb_infak` (`id_infak`, `nama`, `keterangan`, `tanggal`, `jumlah`) VALUES
-(5, 'yusi', 'sedekah', '14-05-2020 22:34:22', '500000'),
-(7, 'putri', 'infak', '14-05-2020 22:36:16', '50000'),
-(8, 'yahdi almukaram', 'infak', '14-05-2020 22:44:04', '20000'),
-(9, 'udin', 'sedekah', '14-05-2020 22:46:57', '500000');
+(67, 'yahdi ', 'sedekah', '04/08/2020 3:35 AM', '500000'),
+(69, 'nadia', 'sedekah', '05/09/2020 3:34 AM', '500000'),
+(71, 'nadia anggi', 'sedekah', '05/09/2020 3:34 AM', '500000'),
+(75, 'fitri', 'sedekah', '21-05-2020 19:24:58', '500000'),
+(76, 'yahdi almukaram', 'sedekah', '21-05-2020 19:39:40', '1000000'),
+(96, 'ardiman', 'kotak amala minggu lalu', '25-05-2020 20:23:42', '1000000'),
+(97, 'yusi hadriyani', 'infak ', '25-05-2020 20:25:17', '5000000'),
+(98, 'nadia', 'sedekah', '25-05-2020 20:29:43', '10000'),
+(99, 'yahdi almukaram', 'sedekah', '25-05-2020 20:46:54', '500000'),
+(104, 'lutvi furkon', 'sedekah', '04/28/2020 1:54 AM', '500000'),
+(105, 'nadia', 'infak ', '05/26/2020 2:05 AM', '500000'),
+(107, 'yusi', 'sedekah', '05/29/2020 2:08 AM', '500000'),
+(108, 'yusi hadnriyani', 'sedekah', '05/29/2020 2:08 AM', '500000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_kegiatan`
+--
+
+CREATE TABLE `tb_kegiatan` (
+  `id_kegiatan` int(11) NOT NULL,
+  `kegiatan` varchar(255) NOT NULL,
+  `tanggal` varchar(50) NOT NULL,
+  `jumlah` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_kegiatan`
+--
+
+INSERT INTO `tb_kegiatan` (`id_kegiatan`, `kegiatan`, `tanggal`, `jumlah`) VALUES
+(5, 'korban', '05/09/2020 3:34 AM', '500000'),
+(6, 'gotoroyong', '05/09/2020 3:34 AM', '500000');
 
 -- --------------------------------------------------------
 
@@ -79,6 +109,12 @@ ALTER TABLE `tb_infak`
   ADD PRIMARY KEY (`id_infak`);
 
 --
+-- Indeks untuk tabel `tb_kegiatan`
+--
+ALTER TABLE `tb_kegiatan`
+  ADD PRIMARY KEY (`id_kegiatan`);
+
+--
 -- Indeks untuk tabel `tb_user`
 --
 ALTER TABLE `tb_user`
@@ -92,7 +128,13 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT untuk tabel `tb_infak`
 --
 ALTER TABLE `tb_infak`
-  MODIFY `id_infak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_infak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_kegiatan`
+--
+ALTER TABLE `tb_kegiatan`
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user`
